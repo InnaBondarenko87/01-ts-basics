315e03d2500adc08a6127bebf62b61631ef2685d
Inna Bondarenko <@email.com>
2025-05-09 19:03:33 +0200

    commit

--- src/task-5.ts
+++ src/task-5.ts
@@ -1,8 +1,6 @@
 type Status = "loading" | "success" | "error";
 
-// let status: Status = "loading";
-
-function logStatus(status: Status) {
+function logStatus(status: Status): void {
   if (status === "loading") {
     console.log("Loading...");
   } else if (status === "success") {
